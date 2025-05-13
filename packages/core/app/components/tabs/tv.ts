d988a4570c25002cebd922a6ec02b1bb9857079c
Wester <wester@choiceform.com>
2025-05-13 21:13:03 +0800

    Update package version to 0.2.9, enhance Storybook preview, and refactor tabs component styles
    - Bumped package version in package.json to 0.2.9.
    - Added preconnect link for improved performance in Storybook preview.
    - Refactored styles in tabs component for better layout and consistency.

--- packages/core/app/components/tabs/tv.ts
+++ packages/core/app/components/tabs/tv.ts
@@ -10,11 +10,11 @@ export const tabsTv = tv({
     ],
     tab: [
       // Layout
-      "h-6 relative grid items-center gap-1",
+      "relative grid h-6 items-center gap-1",
       // Shape
-      "rounded-md tracking-md leading-md",
+      "tracking-md leading-md rounded-md",
       // Spacing
-      "px-2 border border-solid border-transparent",
+      "border border-solid border-transparent px-2",
       // Typography
       "cursor-default",
       // Focus
@@ -22,17 +22,17 @@ export const tabsTv = tv({
     ],
     label: [
       // Layout
-      "row-start-1 col-start-1",
+      "col-start-1 row-start-1",
       // Typography
       "tracking-md",
       // Visibility
-      "aria-hidden:font-strong aria-hidden:invisible",
+      "aria-hidden:invisible aria-hidden:font-medium",
     ],
   },
   variants: {
     active: {
       true: {
-        tab: "bg-secondary-background font-strong text-default-foreground",
+        tab: "bg-secondary-background text-default-foreground font-medium",
       },
       false: {
         tab: "text-secondary-foreground",
