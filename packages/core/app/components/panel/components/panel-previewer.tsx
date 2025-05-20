d608f91dd4b0dc671b4e73d8d4e45664e6939e0d
黎志刚 <lizhigang@MacBook-Pro.local>
2025-05-20 13:34:35 +0800

    fix: PanelPreviewer

--- packages/core/app/components/panel/components/panel-previewer.tsx
+++ packages/core/app/components/panel/components/panel-previewer.tsx
@@ -1,5 +1,16 @@
-export const PanelPreviewer = () => {
+import { ComponentProps } from "react"
+import { tcx } from "~/utils"
+
+type Props = ComponentProps<"div">
+
+export const PanelPreviewer = ({ className, ...rest }: Props) => {
   return (
-    <div className="bg-secondary-background text-secondary-foreground relative mx-4 my-2 flex h-32 flex-col items-center justify-center rounded-xl"></div>
+    <div
+      className={tcx(
+        "bg-secondary-background text-secondary-foreground relative mx-4 my-2 flex h-32 flex-col items-center justify-center rounded-xl",
+        className,
+      )}
+      {...rest}
+    />
   )
 }
