193c931a7690a3c518e4985d95b248eabd8134cf
Wester <wester@choiceform.com>
2025-11-14 03:21:57 +0800

    chore(core): bump version to 1.2.66 and streamline ContextInput exports
    - Updated package version to 1.2.66.
    - Simplified exports in ContextInput component by using wildcard exports for components and types.

--- packages/core/app/components/context-input/index.ts
+++ packages/core/app/components/context-input/index.ts
@@ -1,16 +1,6 @@
 export { ContextInput } from "./context-input"
-export { CopyButton, Mention, type MentionProps } from "./components"
-export type {
-  ContextInputProps,
-  ContextInputValue,
-  MentionItem,
-  MentionMatch,
-  MentionTrigger,
-  ContextMentionElement,
-  ContextParagraphElement,
-  ContextInputElement,
-  ContextInputText,
-} from "./types"
+export * from "./components"
+export type * from "./types"
 export { contextInputTv } from "./tv"
 export { useMentions } from "./hooks/use-mentions"
 export {
